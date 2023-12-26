9dd1078d636a55caff9fc6b4f5acc8590b8d9b45
Christian Programmer i e "Dreamer <117502862+175M3H3RE@users.noreply.github.com>
2023-12-27 03:40:25 +0530

    Update list_env.bat

--- list_env.bat
+++ list_env.bat
@@ -1,28 +1,28 @@
 @echo off
 
-rem *************************************************
-rem |  Program Logic: Echo %Path% by delimiter ";"  |
-rem |                 separated by new line         |
-rem |                 Increase a variable var for   |
-rem |_________________every_line_emitted_else_leave |
+rem **************************************************.
+rem |  Program Logic: Echo %Path% by delimiter ";"    |
+rem |                 separated by new line           |
+rem |                 Increase a variable _lmYExo for |
+rem |_________________every_line_emitted_else_leave___|
 
 
 
-set /a str=1
-set /a var=0
-set /a oldvar=0
-
+set /a _lMKcmi=1
+set /a _lmYExo=0
+set /a _rLdlGF=0
+set /a _jzIOXtES=1
 :loop
-for /f "tokens=%token_no% delims=;" %%i in ("%path%") do CALL :check "%%i"&(echo %%i)
+for /f "tokens=%_jzIOXtES% delims=;" %%i in ("%path%") do CALL :add_lmYExo "%%i"&(echo %%i)
 
-set /a token_no+=1
+set /a _jzIOXtES+=1
 
-REM *** if var is not updating any more exit
-if %oldvar%==%var% goto :eof
+REM *** if _lmYExo is not updating any more exit
+if %_rLdlGF%==%_lmYExo% goto :eof
 
-REM *** make oldvar to be var
-set /a oldvar=var
+REM *** make _rLdlGF to be _lmYExo
+set /a _rLdlGF=_lmYExo
 
 goto loop
-:addvar
-set /a var+=1
+:add_lmYExo
+set /a _lmYExo+=1
